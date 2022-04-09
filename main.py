30d55f5ae000e06fd966ffe18c3c2f1254ceba76
DannyLeWasTaken <dannylewastaken@protonmail.com>
2022-04-09 19:29:59 -0400

    Removed unnceccesary code

--- main.py
+++ main.py
@@ -45,84 +45,12 @@ for user_id in user_pixels_placed.keys():
             difference_in_time.append(placement_time_list[i - 1], placement_time_list[i])
         # Compare differences between the pixels placed by dividing idk if this is a good way or hella inefficient probably is
         average = sum(difference_in_time) / len(difference_in_time)
+        variance = []
+        for time in difference_in_time:
+            variance.append(time / average)
+        user_placement_variance[user_id] = sum(variance) / len(variance)
 
-
-threshold = 0.2 # in seconds
-
-count_dict = {}
-variance_dict = {}
-time_since_dict = {}
-last_pixel_time_dict = {}
-thing1 = []
-thing2 = []
 
 # I don't know what they were doing here nor do I wanna think about it.
 
 print("Processing data for bot detection...")
-
-
-
-"""
-for row in part.itertuples():
-    user_id = row[user_id_idx]
-    if user_id in last_pixel_time_dict.keys():
-        if user_id in time_since_dict.keys():
-            # Get time difference between last pixel placement and newly found pixel
-            time_since_last_pixel = row[timestamp_idx] - last_pixel_time_dict[row[user_id_idx]]
-            variance = abs((time_since_dict[user_id] - time_since_last_pixel).total_seconds())
-            if variance < threshold:
-                if user_id not in count_dict.keys():
-                    count_dict[user_id] = 0
-                count_dict[user_id] += 1
-            else:
-                time_since_dict[user_id] = time_since_last_pixel
-        else:
-            time_since_dict[user_id] = row[timestamp_idx] - last_pixel_time_dict[user_id]
-    # Store current pixel placement
-    last_pixel_time_dict[user_id] = row[timestamp_idx]
-"""
-print("Done!")
-
-# Scans whole data set, disabled for testing purposes
-
-"""
-
-count_dict = {}
-variance_dict = {}
-time_since_dict = {}
-last_pixel_time_dict = {}
-thing1 = []
-thing2 = []
-
-for partit in ddf.partitions:
-    partit["timestamp"] = dd.to_datetime(partit["timestamp"])
-    for row in partit.itertuples():
-        user_id = row[user_id_idx]
-        if user_id in last_pixel_time_dict.keys():
-            if user_id in time_since_dict.keys():
-                time_since_last_pixel = row[timestamp_idx] - last_pixel_time_dict[row[user_id_idx]]
-                variance = abs((time_since_dict[user_id] - time_since_last_pixel).total_seconds())
-                if variance < threshold:
-                    if user_id not in count_dict.keys():
-                        count_dict[user_id] = 0
-                    count_dict[user_id] += 1
-                else:
-                    time_since_dict[user_id] = time_since_last_pixel
-            else:
-                time_since_dict[user_id] = row[timestamp_idx] - last_pixel_time_dict[user_id]
-        last_pixel_time_dict[user_id] = row[timestamp_idx]
-
-"""
-
-print("Generating result files...")
-
-with open('counts.txt', 'w') as convert_file:
-    convert_file.write(json.dumps(count_dict))
-count_series = pd.Series(count_dict)
-
-
-# Finalize output
-with open('counts_greater_than_2.txt', 'w') as convert_file:
-    convert_file.write(json.dumps(count_series[count_series > 2].to_dict()))
-with open('user_ids_greater_than_2.txt', 'w') as outfile:
-    json.dump(list(count_series[count_series > 2].to_dict().keys()), outfile)
